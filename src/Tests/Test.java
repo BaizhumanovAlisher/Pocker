4cfe3e321ba0b98e757366da0d68761f031d5974
Alisher <104581789+Loading58327@users.noreply.github.com>
2022-10-28 00:13:54 +0600

    2 commit
    ~ new tests
    ~ method getNumberRank

--- src/Tests/Test.java
+++ src/Tests/Test.java
@@ -6,8 +6,6 @@ import Main.PokerData.Face;
 import Main.PokerData.HandRank;
 import Main.PokerData.Suit;
 
-import java.util.Objects;
-
 public class Test {
     public static void main(String[] args) {
         Test test = new Test();
